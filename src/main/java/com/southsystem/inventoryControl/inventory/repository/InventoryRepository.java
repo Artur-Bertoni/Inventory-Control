94f65b09480376f6a81c8d08ed1ed73149e0d441
arturSantos <artur.santos@southsystem.com.br>
2023-06-14 17:24:42 -0300

    Commit 3

--- src/main/java/com/southsystem/inventoryControl/inventory/repository/InventoryRepository.java
+++ src/main/java/com/southsystem/inventoryControl/inventory/repository/InventoryRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface InventoryRepository extends MongoRepository<Inventory, Object> {
+public interface InventoryRepository extends MongoRepository<Inventory, String> {
 }
